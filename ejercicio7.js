082f9d8b1105fce69f6735fea2168c4682c99121
DavidRCh56 <davricchi@gmail.com>
2025-03-28 13:03:40 +0100

    intercalarArray hecho

--- ejercicio7.js
+++
@@ -1,2 +0,0 @@
-//crea un array que intercale dos arrays dados.
-//por ejemplo dados [a,b,c,d] y [1,2,3,4] el resultado seria [a,1,b,2,c,3,d,4]
---
+++ intercalarArrayEjercicio7.js
@@ -0,0 +1,28 @@
+//crea un array que intercale dos arrays dados.
+//por ejemplo dados [a,b,c,d] y [1,2,3,4] el resultado seria [a,1,b,2,c,3,d,4]
+//gestionar tambien si un array es mas largo que el otro
+
+const letras = ["a","b","c","d"];
+const numeros = [1,2,3,4,5,6,4,3,2];
+
+function intercalarArray(letras,numeros) {
+    let arrayIntercalado = [];
+    let longitudMaxima = 0;
+
+    if(letras.length > numeros.length){
+        longitudMaxima = letras.length
+    }else{
+        longitudMaxima = numeros.length
+    }
+
+    for (let i = 0; i < longitudMaxima; i++) {
+        if(letras.length > i)
+            arrayIntercalado.push(letras[i]);
+        if(numeros.length > i)
+            arrayIntercalado.push(numeros[i]);
+    }
+    
+    return arrayIntercalado;
+}
+
+console.log(intercalarArray(letras,numeros));
