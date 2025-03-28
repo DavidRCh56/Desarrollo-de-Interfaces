5fafa1d72c89301456d53d4c328b276eeffd1dca
DavidRCh56 <davricchi@gmail.com>
2025-03-28 13:25:19 +0100

    comprobarPalindromo hecho

---
+++ cadenaPalindromoEjercicio9.js
@@ -0,0 +1,31 @@
+//crea un afuncion que determine si una cadena es un palindromo 
+//es decir que sea igual al derecho y al reves
+
+const palindromo = "radar";
+const noPalindromo = "hola";
+
+function comprobarPalindromo(cadena) {
+    let arrayDerecho = [];
+    let arrayReves = [];
+    const longitud = cadena.length;
+
+    // combierto la cadena en array para darle la vuelta mas facil
+    for (let i = 0; i < cadena.length; i++) {
+        arrayDerecho.push(cadena[i]);
+    }
+
+    //le doy la vuelta al array creado
+    for (let i = arrayDerecho.length - 1; i >= 0; i--) {
+        arrayReves.push(arrayDerecho[i]);
+    }
+    
+    //recorre el array hasta que encuentre un valor distinto, si lo encuentra da false dando a entender que no es palindromo
+    for (let j = 0; j < longitud; j++) {
+        if(arrayDerecho[j] !== arrayReves[j])
+            return false;
+    }
+    return true;
+}
+
+console.log(comprobarPalindromo(palindromo));
+console.log(comprobarPalindromo(noPalindromo));
--- ejercicio9.js
+++
@@ -1,2 +0,0 @@
-//crea un afuncion que determine si una cadena es un palindromo 
-//es decir que sea igual al derecho y al reves
