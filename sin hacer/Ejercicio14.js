3bfee01718f5c26432788da10704373f6b4269d4
DavidRCh56 <davricchi@gmail.com>
2025-04-09 01:36:18 +0200

    contadorFraPalLetrEjercicio14

--- sin hacer/Ejercicio14.js
+++
@@ -1,2 +0,0 @@
-//pdf1 javaScript pg101: 1-15
-//6. Crea una función que cuente las frases, palabras y letras presentes en un texto.
---
+++ sin hacer/contadorFraPalLetrEjercicio14.js
@@ -0,0 +1,43 @@
+//pdf1 javaScript pg101: 1-15
+//6. Crea una función que cuente las frases, palabras y letras presentes en un texto.
+
+const texto = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ullam ut maiores pariatur nobis adipisci officiis tempore. Perferendis nam sint vero, voluptatem nostrum nesciunt aspernatur ad deserunt velit voluptatibus libero.";
+
+function contarTexto(texto) {
+    let contadorFrases = 0;
+    let contadorPalabras = 0;
+    let contadorLetras = 0;
+    let palabra = [];
+
+    for (const n of texto) {
+        let caracter = n;
+
+        //si el cararcter no es espacio, punto o coma se añade al array palabra
+        if (caracter !== " " && caracter !== "," && caracter !== ".") {
+            palabra.push(caracter);
+        } else {
+            
+            //si el caracter es espacio, punto o coma y la longitud del array palabra es mayor a 0 
+            //se suma uno al contador de palabras
+            if (palabra.length > 0) {
+                contadorPalabras++;
+                //aprovecho que aqui hay una palabra completa, para añadir la longitud de la misma 
+                //al contador de letras para asi contar todas las letras segun las palabras
+                contadorLetras += palabra.length;
+                //reinicio el array para la siguiente palabra
+                palabra = [];
+            }
+
+            //suma 1 al contador de frases por cada . que se encuentre
+            if (caracter === ".") {
+                contadorFrases++;
+            }
+        }
+    }
+
+    console.log("Frases: " + contadorFrases);
+    console.log("Palabras: " + contadorPalabras);
+    console.log("Letras: " + contadorLetras);
+}
+
+contarTexto(texto);
